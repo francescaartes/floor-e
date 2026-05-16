4d3d1acbfb523c1814ea418139f61466bd354103
Francesca Artes <122511777+francescaartes@users.noreply.github.com>
2026-05-16 16:58:37 +0800

    feat: add non-blocking WiFi and TLS-secured MQTT connection handlers

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,4 +1,7 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
 #include <ESP32Servo.h>
 
 #include "secrets.h"
@@ -12,11 +15,59 @@ const int RIGHT_IN4_PIN = 21;
 const uint16_t PWM_FREQUENCY_HZ = 1000;
 const int DRIVE_SPEED = 220;
 
+// Network Config
+const char *MQTT_HOST = "88851ab995354e9da75db5b5a3e5560b.s1.eu.hivemq.cloud";
+const uint16_t MQTT_PORT = 8883;
+const char *MQTT_TOPIC = "robot/drive";
+
+const unsigned long WIFI_RETRY_INTERVAL_MS = 5000;
+const unsigned long MQTT_RETRY_INTERVAL_MS = 5000;
+
+WiFiClientSecure espClient;
+PubSubClient mqttClient(espClient);
+
 // Hardware timers
 ESP32PWM leftForwardPwm;
 ESP32PWM leftReversePwm;
 ESP32PWM rightForwardPwm;
 ESP32PWM rightReversePwm;
+
+unsigned long lastWifiAttemptAt = 0;
+unsigned long lastMqttAttemptAt = 0;
+
+// Network Functions
+void connectWifi()
+{
+    if (WiFi.status() == WL_CONNECTED)
+        return;
+    const unsigned long now = millis();
+    if (now - lastWifiAttemptAt < WIFI_RETRY_INTERVAL_MS)
+        return;
+
+    lastWifiAttemptAt = now;
+    Serial.print("Connecting to Wi-Fi: ");
+    Serial.println(WIFI_SSID);
+    WiFi.disconnect();
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+}
+
+void connectMqtt()
+{
+    if (WiFi.status() != WL_CONNECTED || mqttClient.connected())
+        return;
+    const unsigned long now = millis();
+    if (now - lastMqttAttemptAt < MQTT_RETRY_INTERVAL_MS)
+        return;
+
+    lastMqttAttemptAt = now;
+    String clientId = "floor-e-esp32-" + String(static_cast<uint32_t>(ESP.getEfuseMac()), HEX);
+
+    if (mqttClient.connect(clientId.c_str(), MQTT_USERNAME, MQTT_PASSWORD))
+    {
+        Serial.println("MQTT connected");
+        mqttClient.subscribe(MQTT_TOPIC);
+    }
+}
 
 // Motor Functions
 void writePwm(ESP32PWM &pwm, int speed)
@@ -74,10 +125,28 @@ void setup()
 {
     Serial.begin(115200);
     delay(100);
+
     setupPwm(); // Initialize tracks on boot
+
+    // Setup Network
+    WiFi.mode(WIFI_STA);
+    espClient.setInsecure();
+    mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+    mqttClient.setKeepAlive(15);
+
+    lastWifiAttemptAt = millis() - WIFI_RETRY_INTERVAL_MS;
+    lastMqttAttemptAt = millis() - MQTT_RETRY_INTERVAL_MS;
+
     Serial.println("System Booting...");
 }
 
 void loop()
 {
+    connectWifi(); // Check Wifi every frame
+    connectMqtt(); // Check MQTT every frame
+
+    if (mqttClient.connected())
+    {
+        mqttClient.loop(); // MQTT loop
+    }
 }
